9868e15c42c6742f852a130c51b384d118fce16e
a-burkholder <a-burkholder.2@onu.edu>
2024-05-14 20:44:51 -0400

    Input Processing function V1
    First version of the input processing command. It is untested but should work well. The main issues are likely with indexing.

--- Sensor_Node_General_Topology/Sensor_Node_General_Topology.ino
+++ Sensor_Node_General_Topology/Sensor_Node_General_Topology.ino
@@ -1,6 +1,8 @@
 
+
+
 const int TOTAL_NODES = 10; // Total number of nodes in the network
-const int ID = 4; // Each node knows its ID based on assumption
+const String ID = "04"; // Each node knows its ID based on assumption
 const int TIME_SLOT = 500; // amount of time per slot in milliseconds (ms) 10^-3
 const int ERROR = 60; // Transmission time error threshold
 const int ENERGY_CHANCE = 80; // energy harvest rate
@@ -10,10 +12,11 @@ const int ENERGY_CHANCE = 80; // energy harvest rate
 bool led_state = false;
 bool trans = false;
 bool firstFlag = false;
-bool is_Sync = false;
+bool is_sync = false;
 
 
 /* Timers */
+unsigned long cycle_time = 0;
 unsigned long sync_time = 0;
 unsigned long current_time = 0;
 unsigned long last_time = 0;
@@ -21,6 +24,8 @@ unsigned long wait_time = 0;
 
 /* Transmition stuff */
 String incoming = ""; 
+String data_in = "";
+String time_in = "";
 
 bool energyAvailible();
 void stateMachine();
@@ -113,21 +118,39 @@ void stateMachine(){
 // readData()
 // Helper function that updates the variables that hold the data. Created to simplify code. (and improve efficency)
 void readData(){
-Serial.println("S" + (String)ID);
+Serial.println("S" + (String)ID.toInt());
   //--Read the data--//
   if(Serial.available()){
     String incoming = Serial.readStringUntil('\r'); 
   }
 
   //--Check for sync or not--//
-  String temp = incoming.substring(0,2); //grab first two things
-  if(temp == "S " || temp == "S" + (String)ID){  //if data is a sync command
-    is_Sync = true;  
-  }
-  else{
+  String sync_flag = incoming.substring(0,1); //grab first character
 
+  if (sync_flag == "G"){  //if general sync 
+    is_sync = true;
+    time_in = incoming.substring(1,incoming.length());//maybe broken bc bad indexing
   }
-  
+
+  else if(sync_flag == "S"){  //if normal sync 
+    int num_syncs = incoming.substring(1,3).toInt(); //number of nodes we need to search through
+    //--linear search through all node IDs--//   REPLACE WITH BINARY SEARCH EVENTUALLY
+    for(int i = 3; i < (num_syncs*2)+3; i+=2){
+      String to_check = incoming.substring(i, i+2);
+      if(to_check == ID){ //if this node finds it's ID on the sync list
+        is_sync = true;
+        time_in = incoming.substring((num_syncs*2)+3, incoming.length());
+        break;
+      }
+    }
+  }
+
+  else{ //if not sync 
+    is_sync = false;
+    int data_idx = incoming.indexOf(',',2); //the index of the comma separating the data from the cycle time
+    time_in = incoming.substring(1,data_idx);
+    data_in = incoming.substring(data_idx + 1, incoming.length());
+  }
 }
 
 
