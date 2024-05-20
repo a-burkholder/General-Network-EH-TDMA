426b44b2d890c61bd2b8ca0f6e46fbf065c35e61
a-burkholder <a-burkholder.2@onu.edu>
2024-05-20 19:18:24 -0400

    Active Finished
    Also verified the functionality of WAIT, ACTIVE, and cycleTime()

--- Sensor_Node_General_Topology/Sensor_Node_General_Topology.ino
+++ Sensor_Node_General_Topology/Sensor_Node_General_Topology.ino
@@ -6,7 +6,7 @@ const int TIME_SLOT = 500; // amount of time per slot in milliseconds (ms) 10^-3
 const unsigned long CYCLE_LENGTH = TOTAL_NODES*TIME_SLOT; // total length of one cycle
 const int ERROR = 60; // Transmission time error threshold
 const String ID = "04"; // Each node knows its ID based on assumption
-const int ENERGY_CHANCE = 80; // energy harvest rate
+const int ENERGY_CHANCE = 1000; // energy harvest rate
 
 
 
@@ -21,22 +21,19 @@ bool is_sync = false; // keeps track of if the last read message was a sync
 unsigned long transmit_time; // time in the cycle to transmit
 unsigned long global_time = 0; // global cycle time from the last sync message
 long offset = 0; // offset from the node's cycle to the global cycle
-
 unsigned long start_clock = 0; // the current node's equivalent time to the global cycle time
-
-
-
-
-
-unsigned long sync_time = 0; // unused
-unsigned long current_time = 0; // unused
-
+unsigned long last_time; // the time at the last time it was checked
 
 
 /* Transmition stuff */
-String incoming = ""; // holds whole input
-String data_in = ""; // the data coming in
-String time_in = ""; // the global cycle time coming in
+String incoming = "Placeholder data input string"; // holds whole input
+String data_in = "Placeholder data"; // the data coming in
+String time_sent = "1500"; // the global cycle time coming in
+unsigned long time_in = "1500"; // time that this node received the message, ideally same as time_sent
+
+bool is_sent = false; // have you sent a message this cycle
+int clock_diff; // used for calculating overlap
+int is_overlap; // to hold the overlap info: 1 for behind, 2 for ok, 3 for ahead
 
 bool energyAvailible();
 void stateMachine();
@@ -53,8 +50,7 @@ void setup() {
 
 void loop() {
   // put your main code here, to run repeatedly:
-  \
-
+  nodeFSM();
 }
 
 
@@ -70,7 +66,7 @@ bool energyAvailible(){
 // nodeFSM()
 // The function that implements the states and their functionality in the nodes
 void nodeFSM(){
-  static enum { DEAD, SYNC, WAIT, ACTIVE } state = DEAD;
+  static enum { DEAD, SYNC, WAIT, ACTIVE } state = WAIT;
   switch (state) {
     case DEAD:
       if(energyAvailible()) {
@@ -83,12 +79,13 @@ void nodeFSM(){
       // read input stream
       if(!updated){
         if(readData()){
+          time_in = cycleTime();
           updated = true;
         }
       }
       
       if(updated){
-        global_time = time_in.toInt(); // sync based on message
+        global_time = time_sent.toInt(); // sync based on message
         start_clock = millis();
         offset = ((int)global_time - (int)start_clock);
         state = WAIT;
@@ -97,17 +94,20 @@ void nodeFSM(){
       updated = false;
       break;
 
-    case WAIT:
+    case WAIT: // -- Verified working
       // check if in time slot
-      if(cycleTime() >= transmit_time){
+      if(cycleTime() >= transmit_time && !is_sent){
         state = ACTIVE;
         break;
       }
       
       // if not in time slot
+      
       else{
+        Serial.println("Waiting...");
         if(readData()){
           updated = true;
+          time_in = cycleTime();
           if (is_sync){
             state = SYNC;
             break;
@@ -116,10 +116,25 @@ void nodeFSM(){
       }  
       break;
 
-    case ACTIVE:
+    case ACTIVE: // -- Verified working
       // check for overlap errors 
+      clock_diff = ((String)time_in).compareTo(time_sent); 
+      if(clock_diff > ERROR){ // behind
+        is_overlap = 1;
+      }
+      else if (clock_diff > -ERROR){ // ok
+        is_overlap = 2;
+      }
+      else { // ahead
+        is_overlap = 3; 
+      }
+
       // update the message
+      String message = "D" + (String)cycleTime() + ID + (String)is_overlap + data_in;
+
       // send the message
+      Serial.println(message);
+      is_sent = true;
 
       // energy checking
       if(energyAvailible()){
@@ -130,7 +145,6 @@ void nodeFSM(){
         state = DEAD;
         break;
       }
-
       break;
   }
 }
@@ -150,7 +164,7 @@ bool readData(){
     String sync_flag = incoming.substring(0,1); //grab first character
     if (sync_flag == "G"){  //if general sync 
       is_sync = true;
-      time_in = incoming.substring(1,incoming.length()); //maybe broken bc bad indexing
+      time_sent = incoming.substring(1,incoming.length()); //maybe broken bc bad indexing
     }
 
     else if(sync_flag == "S"){  //if normal sync 
@@ -160,7 +174,7 @@ bool readData(){
         String to_check = incoming.substring(i, i+2);
         if(to_check == ID){ //if this node finds it's ID on the sync list
           is_sync = true;
-          time_in = incoming.substring((num_syncs*2)+3, incoming.length());
+          time_sent = incoming.substring((num_syncs*2)+3, incoming.length());
           break;
         }
       }
@@ -169,7 +183,7 @@ bool readData(){
     else{ //if not sync 
       is_sync = false;
       int data_idx = incoming.indexOf(',',2); //the index of the comma separating the data from the cycle time
-      time_in = incoming.substring(1,data_idx);
+      time_sent = incoming.substring(1,data_idx);
       data_in = incoming.substring(data_idx + 1, incoming.length());
     }
   }
@@ -177,10 +191,16 @@ bool readData(){
   return isMessage;
 }
 
-// cycleTime()
+// cycleTime() -- Verified working
 // helper function to keep the time in the range of one cycle and incorporate the offset
+// also resets the is_sent variable so we can send a new message if we get to a new cycle
 unsigned long cycleTime(){
-  return (millis() + offset) % CYCLE_LENGTH;
+  unsigned long time = (millis() + offset) % CYCLE_LENGTH;
+  if(last_time > time){
+    is_sent = false;
+  }
+  last_time = time;
+  return time;
 }
 
 
